144f39b2d0e698e905054591cfd8511009d13030
justus <130786194+eduardajustus@users.noreply.github.com>
2023-11-14 19:23:06 -0300

    Update CoordenadasDeUmPonto.js

---
+++ CoordenadasDeUmPonto.js
@@ -0,0 +1,7 @@
+function segundo(){
+    var ElementoMensagem = document.getElementById("id2");
+    var x= parseFloat(prompt("Digite a coordenada 1")) ;
+    var y= parseFloat(prompt("Digite a coordenada 2")) ;
+    
+    while(x = y = 0) ;
+}

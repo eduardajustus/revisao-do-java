ee2ac0985597df43045126a08c6bb1ba0cc53f22
justus <130786194+eduardajustus@users.noreply.github.com>
2023-11-14 19:23:21 -0300

    Update FatorialSimples.js

---
+++ FatorialSimples.js
@@ -0,0 +1,17 @@
+function terceiro(){
+    var ElementoMensagem = document.getElementById("id3");
+    
+    var N = parseFloat(prompt("Digite o valor do fatorial")) ;
+    var fatorial = N;
+    var resultado= fatorial;
+    var primeiroMultiplicador= fatorial- 1;
+    for (i=1; i<1; i++){
+       fatorial=fatorial*(N-i) ;
+    }while(N<1){
+        fatorial=fatorial*(N-i)
+
+    }
+     console.log=parseFloat(prompt(resultado)) ;
+    
+}
+    
